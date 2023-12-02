75734178227afc473d3e95e1f35f799e2499c73a
miki55555115 <miki55555115@gmail.com>
2023-12-02 11:12:57 +0100

    style: format

--- 2023/01-1.py
+++ 2023/01-1.py
@@ -2,19 +2,18 @@ with open("input.txt") as f:
     lines = f.read().splitlines()
 
 res = 0
-
 for line in lines:
     num = ""
     for char in line:
         if char.isdigit():
             num += char
             break
-        
+
     for char in line[::-1]:
         if char.isdigit():
             num += char
             break
-    
-    res += int(num) 
-    
+
+    res += int(num)
+
 print(res)
